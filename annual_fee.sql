6683e1d0a28d048aade1c91f28440b0bf054e443
helen-metro <helen.dinh@metrolibrary.org>
2026-04-22 10:24:56 -0500

    added table views for annual fee paying patrons

--- annual_fee.sql
+++ annual_fee.sql
@@ -18,7 +18,29 @@ and notes = 'Annual Fee'
 group by to_char(transdate, 'YYYY-MM')
 order by to_char(transdate, 'YYYY-MM') desc;
 
+/* This part will cover the annual fee transactions*/
 select *
 from patronfiscal_v2
-where notes = 'Annual Fee'
+where (notes like '%Annual%' or notes like '%annual%')
 order by transdate desc;
+
+/* This part will call out all patrons classifed as Annual Fee Paying */
+select *
+from patron_v2
+where bty in (8, 9, 10);
+
+
+/* The merged view of patrons that classified as Annual Fee Paying  */
+select *
+from patron_v2 a
+left join patronfiscal_v2 b on a.patronid = b.patronid
+where bty in (8, 9, 10);
+
+
+/*Transaction log of Annual Fee Paying patrons in pariod of time*/
+select *
+from txlog_v2 t
+left join patron_v2 pa on t.patronid = pa.patronid
+left join patronfiscal_v2 p on t.patronid = p.patronid
+where pa.bty in (8, 9, 10)
+and t.systemtimestamp >= TO_DATE('2025-01-01', 'YYYY-MM-DD');
